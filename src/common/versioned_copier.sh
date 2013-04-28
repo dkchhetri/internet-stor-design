e620e961fd41ac4ae3d99f3b19f70555adb218a5
Dilip Chhetri <chhetri.inside@gmail.com>
2013-04-28 11:21:00 -0700

    add comments

--- src/common/versioned_copier.sh
+++ src/common/versioned_copier.sh
@@ -22,6 +22,7 @@ setup_bkdir() {
     mkdir ${bkdir}/.metadata/index
 }
 
+# get last index file before this time-stamp
 get_index_file () {
     bkdir=$1
     ts=$2
@@ -45,13 +46,13 @@ get_index_file () {
     done
     if [ $use_ts != "none" ]
     then
-        echo ${bkdir}/.metadata/index/file_index.$use_ts
+        echo -n ${bkdir}/.metadata/index/file_index.$use_ts
     fi
 }
 
 sanitize_fname() {
     fname=$1
-    echo `echo $fname|sed -e 's#//\+#/#g' -e 's#/./#/#g'`
+    echo `echo -n $fname|sed -e 's#//\+#/#g' -e 's#/./#/#g'`
 }
 
 copy_files () {
@@ -76,8 +77,46 @@ copy_files () {
     done
 }
 
+lookup_file_entry() {
+    index_file=$1
+    # escape special characters
+    fname=`echo $2|sed -e 's#\.#\\\.#g' -e 's#\+#\\\+#g' -e 's#\*#\\\*#g'`
+    entry=`egrep "^$fname " $index_file`
+    echo -n $entry
+}
+
 delta_copy_files() {
-    echo delta_copy_files not yet implemented
+    locdir=$1
+    bkdir=$2
+    head_index_file=$3
+    cur_ts=$4
+    use_dir=${bkdir}/data/$cur_ts
+    use_index_file=${bkdir}/.metadata/index/file_index.$cur_ts
+    mkdir -p ${use_dir}
+    for ff in `find $locdir -type f`
+    do
+        f_entry=`lookup_file_entry $head_index_file $ff`
+        if [ ! -z "$f_entry" ]
+        then
+            bk_md5=`echo $f_entry|awk '{print $2}'`
+            loc_md5=`md5sum $ff|awk '{print $1}'`
+            if [ $loc_md5 = $bk_md5 ]
+            then
+                echo $f_entry >> $use_index_file
+                continue
+            fi
+        fi
+        bfname=`sanitize_fname ${use_dir}/$ff`
+        dname=`dirname $ff`
+        bdname=`sanitize_fname ${use_dir}/$dname`
+        if [ ! -d $bdname ]
+        then
+            mkdir -p $bdname
+        fi
+        cp $ff $bfname
+        echo $ff `md5sum $bfname` >> $use_index_file
+        echo $ff
+    done
 }
 
 backup_files() {
@@ -86,7 +125,7 @@ backup_files() {
     cur_ts=`date +%s`
     setup_bkdir $bkdir
     head_index_file=`get_index_file $bkdir $cur_ts`
-    if [ -z $head_index_file ]
+    if [ -z "$head_index_file" ]
     then
         copy_files $locdir $bkdir $cur_ts
     else
@@ -117,7 +156,7 @@ if [ $cmd = "sync" ]
 then
     locdir=$2
     bkdir=$3
-    if [ -z $locdir ] || [ -z $bkdir ]
+    if [ -z "$locdir" ] || [ -z "$bkdir" ]
     then
         usage
     fi
