ba512e57e934ec430b2f36b37189d97441def8c6
Dilip Chhetri <chhetri.inside@gmail.com>
2013-05-02 20:24:59 -0700

    versioned_copy: FIX copy-modified-files. Still to FIX: all entries are duplicated

--- src/common/versioned_copier.clj
+++ src/common/versioned_copier.clj
@@ -8,6 +8,9 @@
   (while (not= -1 (.read stream buf 0 bufsize)))
   (apply str (map (partial format "%02x") (.digest digest)))))
 
+(defn now-seconds []
+  (int (/ (System/currentTimeMillis) 1000)))
+
 (defn walk-dir [dir]
   (remove #(.isDirectory %)
     (file-seq (java.io.File. dir))))
@@ -32,7 +35,7 @@
               #(clojure.string/replace % #"file_index." "")
               (index-db-list idxdir))))))]
     (if prev-ts
-      (str idxdir "/index/" prev-ts)
+      (str idxdir "/index/file_index." prev-ts)
       nil)))
 
 (defn mkdir [dir]
@@ -45,17 +48,57 @@
 (defn normalized-path [path]
   (.getCanonicalPath (java.io.File. path)))
 
+(defn open-idx-writer [idx]
+  (clojure.java.io/writer idx))
+
+(defn close-idx [idx]
+  (.close idx))
+
+(defn idx-tbl-add [idx file md5]
+  (.write idx (str file "|" md5 "\n")))
+
 ;; copy one file, making directory if needed
 (defn copy-one-file [src dst]
   (let [parent (.getParent (java.io.File. dst))]
     (mkdir parent)
+    (println (str "Copy: " src " -> " dst))
     (clojure.java.io/copy (clojure.java.io/file src) (clojure.java.io/file dst))))
 
 (defn copy-all-files [locdir bkdir ts]
   (let [data_dir (normalized-path (str bkdir "/data/" ts))
         idx_db (normalized-path (str bkdir "/.metadata/index/file_index." ts))]
        (.createNewFile (java.io.File. idx_db))
-       (with-open [idxwr (clojure.java.io/writer idx_db)]
+       (with-open [idxwr (open-idx-writer idx_db)]
          (doseq [ff (map #(.getCanonicalPath %) (walk-dir locdir))]
            (copy-one-file ff (str data_dir "/" ff))
-           (.write idxwr (str ff " " (md5file ff) "\n"))))))
+           (idx-tbl-add idxwr ff (md5file ff))))))
+
+(defn parse-idx-tbl [file]
+  (with-open [r (clojure.java.io/reader file)]
+    (loop [l (line-seq r)
+           h (hash-map)]
+      (let [s (first l)]
+        (if-not s
+          h
+          (recur (rest l) (assoc h (first (.split #"\|" s)) (second (.split #"\|" s)))))))))
+
+(defn copy-modified-files [locdir bkdir ts old-idx-db]
+  (let [data_dir (normalized-path (str bkdir "/data/" ts))
+        idx_db (normalized-path (str bkdir "/.metadata/index/file_index." ts))
+        itbl (parse-idx-tbl old-idx-db)]
+       (.createNewFile (java.io.File. idx_db))
+       (with-open [new-idx (open-idx-writer idx_db)]
+         (doseq [ff (map #(.getCanonicalPath %) (walk-dir locdir))]
+           (let [bkmd5 (get itbl ff)
+                 locmd5 (md5file ff)]
+             (idx-tbl-add new-idx ff locmd5)
+             (if (not= locmd5 bkmd5)
+               (copy-one-file ff (str data_dir "/" ff))))))))
+
+
+(defn backup-files [locdir bkdir]
+  (let [cur-ts (now-seconds)
+        idx-db (prev-index-db (str bkdir "/.metadata") cur-ts)]
+    (if idx-db
+      (copy-modified-files locdir bkdir cur-ts idx-db)
+      (copy-all-files locdir bkdir cur-ts))))
